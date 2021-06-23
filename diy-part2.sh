dcbc88a1e496320bd24fadfa3b84fc0d0a59c3f3
byxkbyxk <44155082+byxkbyxk@users.noreply.github.com>
2021-06-23 21:17:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,3 +17,4 @@ git clone https://github.com/tty228/luci-app-serverchan.git ./package/lean/luci-
 # git clone https://github.com/DD663663/luci-theme-argon_new.git ./package/lean/luci-theme-argon_new
 git clone https://github.com/sirpdboy/luci-app-advanced.git ./package/lean/luci-app-advanced
 git clone https://github.com/kenzok8/luci-theme-ifit.git ./package/lean/luci-theme-ifit
+git git clone https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/openwrt-passwall
