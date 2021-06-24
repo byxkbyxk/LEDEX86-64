a9cc198438aae0090dff14b919684e5372460dc4
byxkbyxk <44155082+byxkbyxk@users.noreply.github.com>
2021-06-24 19:46:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,9 +12,10 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 git clone https://github.com/tty228/luci-app-serverchan.git ./package/lean/luci-app-serverchan
 # git clone https://github.com/DD663663/luci-theme-argon_new.git ./package/lean/luci-theme-argon_new
 git clone https://github.com/sirpdboy/luci-app-advanced.git ./package/lean/luci-app-advanced
-git clone https://github.com/kenzok8/luci-theme-ifit.git ./package/lean/luci-theme-ifit
+# git clone https://github.com/kenzok8/luci-theme-ifit.git ./package/lean/luci-theme-ifit
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git ./package/lean/luci-theme-infinityfreedom.git
 git clone https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/openwrt-passwall
